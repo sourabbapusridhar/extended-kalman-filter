97d560178ebcf120c42b7ce27b85c6ce621f932c
Sourab Bapu Sridhar <sourabbsridhar@gmail.com>
2020-05-27 23:52:17 +0200

    Vehicle State Estimation Initial Implementation

---
+++ test/test_unscented_kalman_filter.py
@@ -0,0 +1,29 @@
+# Implementation of Unit Tests to Test Unscented Kalman Filter
+
+import unittest
+import numpy as np
+
+import src.kalman_filter as kf
+import src.motion_model as motmod
+import src.generate_plots as plot
+import src.measurement_model as measmod
+
+class TestUnscentedKalmanFilter(unittest.TestCase):
+    """
+    Class to perform Unit Tests on Unscented Kalman Filter
+    
+    """    
+    def setUp(self):
+        print("Setup Called")
+
+    def tearDown(self):
+        print("Tear Down Called")
+
+    def test_1(self):
+        print("Test 1 Called")
+
+    def test_2(self):
+        print("Test 1 Called")
+
+    def test_3(self):
+        print("Test 1 Called")
