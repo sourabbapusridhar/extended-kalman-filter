63f5a8f76e56dacd952d31b94d0880e5776aae09
Sourab Bapu Sridhar <sourabbsridhar@gmail.com>
2021-06-07 16:06:12 +0200

    Typos fixed

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 # Implementation of Testing
 
+import os
 import argparse
 import numpy as np
 from utils import read_yaml, write_yaml
 
-def test(configurationFilePath):
+def test_vehicle_estimator(configurationFilePath):
     """
     Entry point for testing vehicle state estimators.
 
@@ -17,17 +18,20 @@ def test(configurationFilePath):
     -------
     None
     """
+    defaultConfigurationFilePath = str(os.path.abspath("config\\default.yml"))
+    configurationFilePath = str(os.path.abspath(configurationFilePath))
     print("Path to the configuration file is: {}".format(configurationFilePath))
 
-    defaultConfiguration = read_yaml("config\\default.yml")
+    defaultConfiguration = read_yaml(defaultConfigurationFilePath)
     userDefinedConfiguration = read_yaml(configurationFilePath)
 
     print("Default Configuration is as follows: {}".format(defaultConfiguration))
     print("User defined Configuration is as follows: {}".format(userDefinedConfiguration))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Implementation to test various vehicle state estimators")
     parser.add_argument("-c", "--configFilePath", type=str, default="config\\default.yml", help="Path to the configuration file (Default Value: config/default.yml)")
     args = parser.parse_args()
 
-    test(configurationFilePath=args.configFilePath)
+    test_vehicle_estimator(configurationFilePath=args.configFilePath)
