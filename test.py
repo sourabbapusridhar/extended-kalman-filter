f1418a16b55b866dc53be121e7d7ff5cf4c9db23
Sourab Bapu Sridhar <sourabbsridhar@gmail.com>
2021-06-15 11:42:33 +0200

    Added Functionality

--- test.py
+++ test.py
@@ -24,10 +24,38 @@ def test_vehicle_estimator(configurationFilePath):
 
     defaultConfiguration = read_yaml(defaultConfigurationFilePath)
     userDefinedConfiguration = read_yaml(configurationFilePath)
+    overallConfiguration = userDefinedConfiguration.copy()
 
-    print("Default Configuration is as follows: {}".format(defaultConfiguration))
-    print("User defined Configuration is as follows: {}".format(userDefinedConfiguration))
+    for key in ["measurement_configuration", "data_configuration", "output_configuration"]:
+        if key in defaultConfiguration:
+            overallConfiguration.update({key: defaultConfiguration[key]})
 
+    overallModels = overallConfiguration["model_configuration"]["motion_model"].replace(","," ").split()
+    for modelId, model in enumerate(overallModels):
+        
+        overallConfiguration["model_configuration"]["motion_model"] = model
+        overallConfiguration["model_configuration"]["covariance_matrix"] = np.array(userDefinedConfiguration["model_configuration"]["covariance_matrix"], dtype=float)
+        #assert (overallConfiguration["measurement_configuration"]["covariance_matrix"].shape == (overallConfiguration["measurement_configuration"]["measurement_variables"], overallConfiguration["measurement_configuration"]["measurement_variables"])), ("[ERROR] Measurement Covariance Matrix Dimensions are Incorrect!!")
+
+        overallConfiguration["measurement_configuration"]["measurement_variables"] = int(overallConfiguration["measurement_configuration"]["measurement_variables"])
+        overallConfiguration["measurement_configuration"]["covariance_matrix"] = np.array(userDefinedConfiguration["measurement_configuration"]["covariance_matrix"], dtype=float)
+        #assert (overallConfiguration["measurement_configuration"]["covariance_matrix"].shape == (overallConfiguration["measurement_configuration"]["measurement_variables"], overallConfiguration["measurement_configuration"]["measurement_variables"])), ("[ERROR] Measurement Covariance Matrix Dimensions are Incorrect!!")
+
+        overallConfiguration["data_configuration"]["data_directory"] = str(os.path.abspath(overallConfiguration["data_configuration"]["data_directory"]))
+        overallConfiguration["data_configuration"]["data_file_type"] = "CSV" if defaultConfiguration["data_configuration"]["data_file_type"] == "CSV" else None
+        overallConfiguration["data_configuration"]["data_variables"] = int(overallConfiguration["data_configuration"]["data_variables"])
+        #assert (overallConfiguration["measurement_configuration"]["measurement_variables"] == overallConfiguration["data_configuration"]["data_variables"]), ("[ERROR] Mistmatch in number of measurement variables and number of data points")
+
+        overallConfiguration["output_configuration"]["animation"] = True if userDefinedConfiguration["output_configuration"]["animation"] == True else False
+        overallConfiguration["output_configuration"]["save_output"] = True if userDefinedConfiguration["output_configuration"]["save_output"] == True else False
+        overallConfiguration["output_configuration"]["output_directory"] = str(os.path.abspath(overallConfiguration["output_configuration"]["output_directory"]))
+
+        print("***********************************************")
+        print("Current Configuration is as follows: ")
+        for key, value in overallConfiguration.items():
+            print("{}: {}".format(key,value))
+
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Implementation to test various vehicle state estimators")
